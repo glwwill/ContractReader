7945e51ef2e246c703b43ab9fe9d97cfe08ea73a
Gary Williams <glwwill@gmail.com>
2024-08-14 18:04:01 -0700

    render r6

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,8 @@ import type { NextConfig } from 'next'
  
 const nextConfig: NextConfig = {
     webpack: (config) => {
-        config.externals.push('pino-pretty', /* add any other modules that might be causing the error */);
+
+        config.externals.push('pino-pretty', 'encoding', /* add any other modules that might be causing the error */);
         return config;
       },
 }
